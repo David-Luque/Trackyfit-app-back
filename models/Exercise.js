b8b22d0dd548050cc42332b101f0e5800c8656cd
David luque Alba <dvdlq99@gmail.com>
2021-01-20 19:39:18 +0100

    drafted new model for new features

--- models/Exercise.js
+++ models/Exercise.js
@@ -10,6 +10,64 @@ const exerciseSchema = new Schema({
   owner: {type: Schema.Types.ObjectId, ref: 'User'}
 })
 
+
+// const exerciseSchema = new Schema ({
+//   name: {type: String},
+//   data: {type: [data]},
+//   owner: {type: Schema.Types.ObjectId, ref: 'User'}
+// })
+
+// const data = new Schema ({
+//   value: {type: Number},
+//   date: {type: Date}
+// })
+
+
+
 const Exercise = mongoose.model('Exercise', exerciseSchema)
 
 module.exports = Exercise
+
+
+// // MODEL SKETCH
+// {
+//   owner: id2343245,
+//   exercises: {
+//     pushup: [
+//       {
+//         value: 12, 
+//         date: 15/07/1993
+//       },{
+//         value: 16, 
+//         date: 16/07/1993
+//       },{
+//         value: 18, 
+//         date: 17/07/1993
+//       }
+//     ],
+//     pullups: [
+//       {
+//         value: 12, 
+//         date: 15/07/1993
+//       },{
+//         value: 16, 
+//         date: 16/07/1993
+//       },{
+//         value: 18, 
+//         date: 17/07/1993
+//       }
+//     ],
+//     squads: [
+//       {
+//         value: 12, 
+//         date: 15/07/1993
+//       },{
+//         value: 16, 
+//         date: 16/07/1993
+//       },{
+//         value: 18, 
+//         date: 17/07/1993
+//       }
+//     ]
+//   }
+// }
--- models/Metrics.js
+++ models/Metrics.js
@@ -10,6 +10,22 @@ const metricSchema = new Schema({
   owner: {type: Schema.Types.ObjectId, ref: 'User'}
 })
 
+// const data = new Schema ({
+//   value: {type: Number},
+//   date: {type: Date}
+// })
+
+// const singleMetric = new Schema ({
+//   name: {type: String},
+//   info: {type: [data]}
+// })
+
+// const metricSchema = new Schema({
+//   owner: {type: Schema.Types.ObjectId, ref: 'User'},
+//   exercises: {type: [singleMetric]}
+// })
+
+
 const Metrics = mongoose.model('Metrics', metricSchema)
 
 module.exports = Metrics
