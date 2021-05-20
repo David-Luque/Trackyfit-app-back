c62890b87826ae1fb5bae1e87ba9e1e99118b80b
David L <dvdlq99@gmail.com>
2021-05-20 09:33:24 +0200

    Fixed auth routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -9,49 +9,63 @@ authRoutes.post('/signup', (req, res) => {
 	const { username, password } = req.body;
 
 	if (!username || !password) {
-		res.send({ errorMessage: 'Provide username and password' });
+		res.status(400).json({ message: 'Provide username and password' });
 		return;
 	}
 
-	if (password < 4) {
-		res.send({ errorMessage: 'Password must be longer' });
+	if (password < 5) {
+		res.status(400).json({ message: 'Password must be at least 6 character long' });
 		return;
 	}
-	User.findOne({ username })
-		.then((user) => {
-			if (!user) {
-				bcrypt.genSalt(10).then((salt) => {
-					bcrypt.hash(password, salt).then((hashedPwd) => {
-						newUser = {
-							username,
-							password: hashedPwd
-						};
-						User.create(newUser)
-							.then(() => {
-								res.send({ errorMessage: 'User successfully registered' });
-							});
-					});
-				});
-			} else {
-				res.send({ errorMessage: 'Username already exist' });
-			}
-		})
-		.catch((err) => {
-			console.error(err);
+
+	User.findOne({ username }, (err, foundUser) => {
+		
+		if(err){
+			res.status(500).json({ message: "Username check error" });
+			return;
+		};
+
+		if(foundUser){
+			res.status(400).json({ message: "Username already exist. Please, choose another one" });
+			return;
+		}
+
+		const salt = bcrypt.genSaltSync(10);
+		const hashPass = bcrypt.hashSync(password, salt);
+
+		const newUser = new User({
+			username: username,
+			password: hashPass
 		});
+
+		newUser.save(err => {
+			if(err){
+				res.status(400).json({ message: "Error saving user to database" });
+				return;
+			};
+
+			req.login(newUser, (err)=>{
+				if(err){
+					res.status(500).json({ message: "Error login after signup" });
+					return;
+				};
+
+				res.status(200).json({ newUser })
+			});
+		});
+	});
 });
 
 
 authRoutes.post('/login', (req, res, next) => {
 	passport.authenticate('local', (err, theUser, failureDetails) => {
 		if (err) {
-			console.log('error 75', err);
-			res.status(500).send({ message: 'Something went wrong authenticating user' });
+			res.status(500).send({ message: 'Something went wrong while authenticating user' });
 			return;
 		}
 
 		if (!theUser) {
-			res.send({message: 'Please, insert valid username and password'});
+			res.status(401).json({ failureDetails });
 			return;
 		}
 
@@ -60,19 +74,19 @@ authRoutes.post('/login', (req, res, next) => {
 				res.status(500).send({ message: 'Session save went bad.' });
 				return;
 			}
-			res.send(theUser);
+			res.status(200).json(theUser);
 		});
 	})(req, res, next);
 });
 
 authRoutes.post('/logout', (req, res, next) => {
 	req.logout();
-	res.status(200).send({ message: 'Log out success! ' });
+	res.status(200).json({ message: 'Log out success! ' });
 });
 
 authRoutes.get('/loggedin', (req, res, next) => {
 	if (req.isAuthenticated()) {
-		res.status(200).send(req.user);
+		res.status(200).json(req.user);
 		return;
 	}
 	res.status(403).send({ message: 'Unauthorized' });
