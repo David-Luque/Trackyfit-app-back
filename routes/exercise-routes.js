783c0646c17b321ed2ad3a27cf9c8203cd882b85
David L <dvdlq99@gmail.com>
2021-05-26 12:54:30 +0200

    created edit exercise route and moved buid files to public

--- routes/exercise-routes.js
+++ routes/exercise-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const Exercise = require('../models/performance_models/Exercise');
 const Result = require('../models/performance_models/Result');
 
+
 router.get('/all-exercises', (req, res, next)=>{
   Exercise.find({ owner: req.user._id })
-  // .populate('results')
   .then(response => res.json(response))
   .catch(err => res.json(err))
 });
@@ -21,11 +22,33 @@ router.post('/create-exercise', (req, res, next)=>{
   .catch(err => res.json(err));
 });
 
-router.put('/edit-exercise', (req, res, next)=>{
-  Exercise.findByIdAndUpdate(req.body.id, { name: req.body.name })
-  .then(response => res.json(response))
+router.put('/edit-exercise/:id', (req, res, next)=>{
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+    res.status(400).json({ message: "Specified 'id' is not valid" });
+  };
+  console.log(req.body)
+  
+  Exercise.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+    //console.log(response)
+    res.json({ message: `Exercise with id: ${req.params.id} was successfully updated` })
+  })
   .catch(err => res.json(err));
 });
 
+
+router.get('/exercises/:id', (req, res, next)=>{
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+    res.status(400).json({ message: "Specified 'id' is not valid" });
+  };
+
+  Exercise.findById(req.params.id)
+  .populate('results')
+  .then(theExercise => {
+    //console.log(theExercise)
+    res.status(200).json(theExercise)
+  })
+  .catch(err => res.json(err))
+});
 
 module.exports = router;
