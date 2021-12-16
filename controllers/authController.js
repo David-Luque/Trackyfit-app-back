55e460404a095eb9cdddb41ce749b5e08f831865
David L <dvdlq99@gmail.com>
2021-12-16 22:43:00 +0100

    Updated signUp route with controller and validation

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const { validationResult } = require('express-validator');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+exports.signUp = async(req, res)=>{
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    
+    if(user) {
+      return res.status(400).json({ msg: 'User already exist' });
+    }
+
+    user = new User(req.body);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(password, salt)
+    user.password = hashPass;
+
+    await user.save();
+
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(payload, process.env.SECRET, {
+      expiresIn: 7200 //2h
+    }, (err, token)=>{
+      if(err) throw err;
+
+      res.json(token);
+    })
+  } catch(err) {
+    console.log(err);
+    res.status(400).send('There was an error while creating user')
+  }
+    
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,63 +1,19 @@
 const express 		= require('express');
-const authRoutes 	= express.Router();
-const passport 		= require('passport');
-const bcrypt 			= require('bcryptjs');
-
-const User 				= require('../models/User');
-
-
-authRoutes.post('/signup', (req, res) => {
-	const { username, password } = req.body;
-
-	if (!username || !password) {
-		res.status(400).json({ message: 'Provide username and password' });
-		return;
-	}
-
-	if (password < 5) {
-		res.status(400).json({ message: 'Password must be at least 6 character long' });
-		return;
-	}
-
-	User.findOne({ username }, (err, foundUser) => {
-		
-		if(err){
-			res.status(500).json({ message: "Username check error" });
-			return;
-		};
-
-		if(foundUser){
-			res.status(400).json({ message: "Username already exist. Please, choose another one" });
-			return;
-		}
-
-		const salt = bcrypt.genSaltSync(10);
-		const hashPass = bcrypt.hashSync(password, salt);
-
-		const newUser = new User({
-			username: username,
-			password: hashPass
-		});
-
-		newUser.save(err => {
-			if(err){
-				res.status(400).json({ message: "Error saving user to database" });
-				return;
-			};
-
-			req.login(newUser, (err)=>{
-				if(err){
-					res.status(500).json({ message: "Error login after signup" });
-					return;
-				};
-				res.status(200).json(newUser)
-			});
-		});
-	});
-});
+const authRouter 	= express.Router();
+const { check } = require('express-validator');
+//const passport 		= require('passport');
+const authController = require('../controllers/authController');
 
 
-authRoutes.post('/login', (req, res, next) => {
+authRouter.post('/signup', [
+	check('username', 'Username is required').not().isEmpty(),
+	check('email', 'Provide a valid email').isEmail(),
+	check('password', 'Password must be at least 6 characters long').isLength({ min: 6 })
+], authController.signUp
+);
+
+
+authRouter.post('/login', (req, res, next) => {
 	passport.authenticate('local', (err, theUser, failureDetails) => {
 		if (err) {
 			res.status(500).json({ message: 'Something went wrong while authenticating user' });
@@ -80,13 +36,13 @@ authRoutes.post('/login', (req, res, next) => {
 });
 
 
-authRoutes.post('/logout', (req, res, next) => {
+authRouter.post('/logout', (req, res, next) => {
 	req.logout();
 	res.status(200).json({ message: 'Log out success! ' });
 });
 
 
-authRoutes.get('/loggedin', (req, res, next)=>{
+authRouter.get('/loggedin', (req, res, next)=>{
   if(req.isAuthenticated()){
     res.status(200).json(req.user);
     return;
@@ -95,4 +51,4 @@ authRoutes.get('/loggedin', (req, res, next)=>{
 });
 
 
-module.exports = authRoutes;
+module.exports = authRouter;
