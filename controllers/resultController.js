a3c7eb931337b4e1fdbf34d11b92b1ad2b088bf5
David L <dvdlq99@gmail.com>
2021-12-20 16:32:09 +0100

    done post result route

--- controllers/resultController.js
+++ controllers/resultController.js
@@ -1,23 +1,31 @@
-const Results = require('../models/performance_models/Result');
+const Result = require('../models/performance_models/Result');
+const Exercise = require('../models/performance_models/Exercise');
 const { validationResult } = require('express-validator');
+const Result = require('../models/performance_models/Result');
 
 exports.createResult = async (req, res)=>{
-  const { reps, time, weight, date, exercise } = req.body;
+  
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() })
+  }
+  
+  try {
+    const { exercise } = req.body;
+    const exerciseDB = await Exercise.findById(exercise);
 
-  Result.create({
-    reps,
-    time, 
-    weight,
-    date,
-    exercise
-  })
-  .then(response => {
-    return Exercise.findByIdAndUpdate(req.body.exercise, {
-      $push: {results: response._id}
-    })
-    .then(theResponse => {
-      res.json(theResponse)
-    })
-  })
-  .catch(err => res.json(err))
+    if(!exerciseDB) res.status(404).json({ msg: "Exercise not found" });
+    if(exerciseDB.owner.toString() !== req.user.id) res.status(401).json({ msg: 'Unauthorized' });
+
+    const result = new Result(req.body);
+    await result.save();
+
+    await Exercise.findByIdAndUpdate(req.body.exercise, { $push: { results: result._id } })
+
+    res.json({ result });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('There was an error while creating')
+  }
 };
--- routes/result.js
+++ routes/result.js
@@ -3,17 +3,30 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const resultController = require('../controllers/resultController');
 //const mongoose = require('mongoose');
-const Result = require('../models/performance_models/Result');
-const Exercise = require('../models/performance_models/Exercise');
 const { check } = require('express-validator');
 
 
 router.post('/', 
   auth, 
   [
-    
+    check('exercise', 'Must be provided a valid exercise id').not().isEmpty()
   ], 
   resultController.createResult
 );
 
+//route to get all results or populate exercise results??
+router.get();
+
+//route to get all data for specific result instead of get all info from all results in route above?
+//In that case, in route above only provide minimal info for display overview info of exercise results
+
+//update result
+router.put('/:id', auth, [], resultController.editResult);
+
+//delete result
+router.delete('/:id', auth, resultController.deleteResult);
+
+
 module.exports = router;
+
+
